35b026c1a0e455bcd05bdc9e3ea6341d5e3de16c
Jason Aricheta <jason.aricheta@gmail.com>
2022-05-11 10:58:54 +1200

    added then/catch to mongoose.connect

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ const Blog = mongoose.model('Blog', blogSchema)
 
 logger.info(`connecting to`, config.MONGODB_URI);
 mongoose.connect(config.MONGODB_URI)
+  .then(() => logger.info('connected to MongoDB'))
+  .catch(error => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.json())
